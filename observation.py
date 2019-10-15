ed1d46a39538ad9391d8f74348a98babbce13736
Robin Chatelet <robinchatelet21@gmail.com>
2019-10-15 13:36:34 +0200

    Create observation.py

---
+++ observation.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 11:13:50 2019
+
+@author: Robin Chatelet & Antoine Branson
+"""
+
+import numpy as np
+import pandas as pd
+import pandas_profiling
+
+df = pd.read_csv(
+        "antivirus_dataset.csv",  delimiter='|', index_col=0
+)
+print(df.sample(5))
